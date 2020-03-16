348882a81fe695461277a9c92dfba30b8d7b1567
yiluheihei <yiluheihei@gmail.com>
2020-03-17 00:21:08 +0800

    raw data

--- data-raw/city_zh_en.R
+++ data-raw/city_zh_en.R
@@ -1,3 +1,4 @@
+# not used in ncovmap right now
 library(reticulate)
 download.file("https://github.com/BlankerL/DXY-COVID-19-Crawler/raw/b92a89ff520f6b35ebe435eafffb386b655f1402/service/nameMap.py", "data-raw/nameMap.py")
 
--- data-raw/correct_ncov-city.R
+++ data-raw/correct_ncov-city.R
@@ -1,47 +1,44 @@
 # correct ncov cities, create city_referece as internal data
 
 library(magrittr)
-library(dplyr)
+# library(dplyr)
 library(purrr)
 library(leafletCN)
 
-download.file("https://github.com/pzhaonet/ncovr/raw/master/inst/china_city_list.csv",
-              destfile = "data-raw/china_city_list.csv")
-china_cities <- readr::read_csv("data-raw/china_city_list.csv")
-# data correction
-china_cities <- dplyr::mutate(china_cities,
-                              Province_EN = dplyr::case_when(
-                                Province_EN == "anhui" ~ "Anhui",
-                                Province_EN == "guizhou" ~ "Guizhou",
-                                Province_EN == "hubei" ~ "Hubei",
-                                Province_EN == "xinjiang" ~ "Xinjiang",
-                                TRUE ~ Province_EN
-                              ))
-readr::write_csv(china_cities, "inst/china_city_list.csv", )
+china_cities <- readr::read_csv("inst/china_city_list.csv")
+# # data correction
+# china_cities <- dplyr::mutate(china_cities,
+#                               Province_EN = dplyr::case_when(
+#                                 Province_EN == "anhui" ~ "Anhui",
+#                                 Province_EN == "guizhou" ~ "Guizhou",
+#                                 Province_EN == "hubei" ~ "Hubei",
+#                                 Province_EN == "xinjiang" ~ "Xinjiang",
+#                                 TRUE ~ Province_EN
+#                               ))
+# readr::write_csv(china_cities, "inst/china_city_list.csv", )
 # usethis::use_data(china_cities, internal = TRUE, overwrite = TRUE)
 
 
 # correct province name
-province <- leafletCN::regionNames("china")
-ncov <- ncovr::get_ncov()
+province_map <- leafletCN::regionNames("china")
+ncov <- get_ncov2()
+# ncov <- readr::read_csv("https://github.com/yiluheihei/nCoV-2019-Data/raw/master/ncov_area.csv")
 # ncov province
-ncov_province <- ncov$provinceName
-need_correct_province <- setdiff(province, ncov_province)
-province[match(need_correct_province, province)] <-
-  c("台湾", "香港", "澳门")
+province_ncov <- ncov$provinceShortName
+province_need_correct <- setdiff(province_map, province_ncov)
+# province[match(province_need_correct, province_map)] <- c("台湾", "香港", "澳门")
 
 # cities does not need info: taiwan, xianggang, aomen
-province_has_cities <-
-  setdiff(province,
-          c("台湾", "香港", "澳门"))
+province_has_cities <- setdiff(province_map, c("台湾", "香港", "澳门", "南海诸岛"))
 cities_list <- purrr::map(province_has_cities, regionNames)
 names(cities_list) <- province_has_cities
-province_cities <-
-  purrr::map2(cities_list, province_has_cities,
-              ~ data.frame(Province = .y, City_Admaster = .x)) %>%
+province_cities <- purrr::map2(
+  cities_list, province_has_cities,
+  ~ data.frame(Province = .y, City_Admaster = .x)
+) %>%
   dplyr::bind_rows() %>%
-  mutate(Province = substr(Province, 1, 2)) %>%
-  mutate(Province = dplyr::case_when(
+  dplyr::mutate(Province = substr(Province, 1, 2)) %>%
+  dplyr::mutate(Province = dplyr::case_when(
     Province == "黑龙" ~ "黑龙江",
     Province == "内蒙" ~ "内蒙古",
     TRUE ~ Province
@@ -56,12 +53,17 @@ province_cities$Province_EN <- china_cities$Province_EN[
 #' @param ncov ncov data
 #' @param p province name
 get_province_cities <- function(ncov, p) {
-  province_dat <- dplyr::filter(ncov$area, provinceName == p)
-  cities <- province_dat$cities[[1]]$cityName
+  province_dat <- dplyr::filter(ncov, provinceShortName == p)
+  cities <- province_dat$cityName %>%
+    unique() %>%
+    na.omit()
 }
 # ncov cities
-ncov_cities_list <- purrr::map(province_has_cities,
-                               get_province_cities, ncov = ncov)
+ncov_cities_list <- purrr::map(
+  province_has_cities,
+  get_province_cities,
+  ncov = ncov
+)
 names(ncov_cities_list) <- province_has_cities
 
 #' distinguish cities between consistent and inconsistent
@@ -116,10 +118,13 @@ city_need_corrected <- tibble::tribble(
   "宁东",                "(宁东)",             "宁夏回族自治区"
 )
 city_need_corrected$type <-  "error"
-city_correct <- purrr::map2(city_distg, province_has_cities,
-                     ~ data.frame(origin = .x$consistent_ncov,
-                                  corrected = .x$consistent,
-                                  Province = .y)) %>%
+city_correct <- purrr::map2(
+  city_distg, province_has_cities,
+  ~ data.frame(
+    origin = .x$consistent_ncov,
+    corrected = .x$consistent,
+    Province = .y)
+) %>%
   dplyr::bind_rows()
 city_correct$type <-  "correct"
 city_reference <- dplyr::bind_rows(city_correct, city_need_corrected)
